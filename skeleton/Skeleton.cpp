8a9d578d2010d0b27277c3e2bbfa4dbb8bb8f261
jywhy6 <jywhy6@icloud.com>
2021-01-21 20:59:28 +0800

    Replace the implementation of SkeletonPass according to DCC888

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,30 +1,54 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 using namespace llvm;
 
+// From
+// https://homepages.dcc.ufmg.br/~fernando/classes/dcc888/ementa/slides/ControlFlowGraphs.pdf
 namespace {
-  struct SkeletonPass : public FunctionPass {
-    static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+struct SkeletonPass : public FunctionPass {
+  static char ID;
+  SkeletonPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
-      return false;
+  virtual bool runOnFunction(Function &F) {
+    std::map<std::string, int> opCounter;
+    static char ID;
+
+    errs() << "opcodes in function " << F.getName() << '\n';
+    for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+      for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
+        if (opCounter.find(i->getOpcodeName()) == opCounter.end()) {
+          opCounter[i->getOpcodeName()] = 1;
+        } else {
+          opCounter[i->getOpcodeName()] += 1;
+        }
+      }
     }
-  };
-}
+    std::map<std::string, int>::iterator i = opCounter.begin();
+    std::map<std::string, int>::iterator e = opCounter.end();
+    while (i != e) {
+      errs() << i->first << ": " << i->second << "\n";
+      i++;
+    }
+    errs() << "\n";
+    opCounter.clear();
+    return false;
+  }
+};
+} // namespace
 
 char SkeletonPass::ID = 0;
+
+static RegisterPass<SkeletonPass> X("skeleton", "Skeleton Pass", false, false);
 
 // Automatically enable the pass.
 // http://adriansampson.net/blog/clangpass.html
 static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
+                                 legacy::PassManagerBase &PM) {
   PM.add(new SkeletonPass());
 }
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+    RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                   registerSkeletonPass);
